f098f29183b3a3224b2735f1ea5edc29ea9dd88a
Benjamin Breton <bbenjaminbreton@gmail.com>
2021-12-07 08:57:14 +0100

    Initial Commit

---
+++ Tic Tac Toe vs Ben/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tic Tac Toe vs Ben
+//
+//  Created by Benjamin Breton on 07/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
